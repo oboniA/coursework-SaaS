45489a0bd535ae81f7a1333fb553c255204ac3eb
Oboni <oanowe01@student.bbk.ac.uk>
2023-11-25 17:04:14 +0000

    Hashed the registered password with bcrypt and salt

--- routes/user-auth.js
+++ routes/user-auth.js
@@ -5,41 +5,53 @@ const router = express.Router()
 // import user Post
 const User = require('../models/User')
 
-// import user-registration-validation
+// import user registration and signin validations
 const {signupValidation, signinValidation} = require('../user-validations/validation')
 
+// import passsword encryption package
+const bcryptjs = require('bcryptjs')
 
-// user registration endpoint
+
+// USER REGISTRATION END-POINT
 router.post('/sign-up', async(req, res)=> {
+
     // validation check for user-input
     const {error} = signupValidation(req.body) 
     // if error in user input
     if(error){
-        return res.status(400).send({message: error.details[0].message}) // status code 400 = invalid syntax
+        return res.status(400).send({message: error.details[0].message}) 
     }
 
-    // else IF no error in user input
-    // code to insert data 
+    // validation check for existing user
+    const userExists = await User.findOne({email:req.body.email}) 
+    // if user exists
+    if(userExists){
+        return res.status(400).send({message: 'User Aready Exists'})
+    }
+
+    // to add additional layer of security
+    // to encrypt password
+    const salt = await bcryptjs.genSalt(5)  // hash password with random no.
+    const hashPass = await bcryptjs.hash(req.body.password,salt)
+
+
+    // else
+    // insert & save data to Database
     const user = new User({
         username:req.body.username,
         email:req.body.email,
-        password: req.body.password
+        password: hashPass
     })
-    // save the data to Database
-    // exception-handling
+
     try{
         const savedUser = await user.save()
         res.send(savedUser)
     }catch(err){
         res.status(500).send({message:'server error'})  // status code 500 = server error
     }
-
-
-
-
 })
 
-// user Sign-in endpoint
+// USER SIGN-IN END-POINT
 router.post('/sign-in', async(req, res)=>{
     console.log(req.body)
 })
